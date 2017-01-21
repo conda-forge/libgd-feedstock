e65a12b4d7a880002da280c1c92613c7ede5f9a5
Christian Roth <christian.roth@mpibpc.mpg.de>
2017-01-21 07:07:07 +0100

    MNT: Re-rendered with conda-smithy 2.0.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,4 +12,7 @@ if [ "$(uname)" == "Darwin" ]; then
     make check || failed=1
     grep -rl "DYLD_LIBRARY_PATH=" tests | xargs sed -i.bak "s~DYLD_LIBRARY_PATH=.*~DYLD_LIBRARY_PATH=$PREFIX/lib~g"
 fi
+
+# see: https://github.com/libgd/libgd/issues/302
+export FREETYPE_PROPERTIES=truetype:interpreter-version=35
 make check || { cat tests/test-suite.log; exit 1; }
