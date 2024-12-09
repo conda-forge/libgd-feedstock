27e87991c5e7657a79de762a753f23286ac3809e
Isuru Fernando <isuruf@gmail.com>
2024-12-09 13:35:54 +0530

    generate a pkgconfig file and install only gd public headers on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ if errorlevel 1 exit 1
 REM done compiling
 
 REM Install step
-copy src\*.h %LIBRARY_INC%\	
+copy src\gd*.h %LIBRARY_INC%\
 if errorlevel 1 exit 1
 cd .\gdbuild
 copy libgd.dll %LIBRARY_BIN%\
@@ -15,4 +15,9 @@ if errorlevel 1 exit 1
 copy libgd.lib %LIBRARY_LIB%\gd.lib
 if errorlevel 1 exit 1
 copy libgd_a.lib %LIBRARY_LIB%\
+if errorlevel 1 exit 1
+
+mkdir %LIBRARY_LIB%\pkgconfig
+if errorlevel 1 exit 1
+sed "s/@VERSION@/${PKG_VERSION}/g" %RECIPE_DIR%\gdlib.pc.in | sed "s#@prefix@#%LIBRARY_PREFIX%@g" > %LIBRARY_LIB%\pkgconfig\gdlib.pc.in
 if errorlevel 1 exit 1
